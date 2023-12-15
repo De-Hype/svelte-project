d47e6185b73a0106bc1c6e4a2a9c973126605ce9
De-Hype <iamsocialhype@gmail.com>
2023-12-15 15:32:53 -0800

    working on pricing section

--- src/lib/Components/Pricing.svelte
+++ src/lib/Components/Pricing.svelte
@@ -1,10 +1,55 @@
 <script>
+    let PricingData = [
+        {
+            'id':1,
+            'name':'Free Trial',
+            'price':10,
+            // 'features':{
+            //     'id':1,
 
+            // },
+        },
+        {
+            'id':2,
+            'name':'Starter',
+            'price':10,
+            // 'features':{
+            //     'id':1,
+
+            // },
+        },
+        {
+            'id':3,
+            'name':'Pro',
+            'price':16,
+            // 'features':{
+            //     'id':1,
+
+            // },
+        },
+    ]
 </script>
 <pricing>
-
+    <div class="container">
+        {#each PricingData as plans (plans.id)}
+        <div class="card">
+            <h3 class="">${plans.price}</h3>
+            <button>{plans.name}</button>
+        </div>
+    {/each}
+    </div>
 </pricing>
 
 <style>
-    
+    .container{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    }
+    .card{
+        border: 1px solid red;
+        display: flex;
+        align-items:center;
+        justify-content: center;
+        flex-direction: column;
+    }
 </style>
