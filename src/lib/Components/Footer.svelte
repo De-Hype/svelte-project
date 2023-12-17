c7d5d7f802747459844c46c54eb55be1dbaba58f
De-Hype <iamsocialhype@gmail.com>
2023-12-17 13:25:48 -0800

    Working on footer components

--- src/lib/Components/Footer.svelte
+++ src/lib/Components/Footer.svelte
@@ -11,7 +11,52 @@
       id eos eius alias quibusdam neque.
     </p>
   </div>
+  <div class="footer-col-two">
+    <h4>Product</h4>
+    <div class="footer-navigations">
+        <p>Home</p>
+        <p>Pricing</p>
+        <p>Testimony</p>
+        <p class="wrapper">Careers <span>Hiring</span></p>
+    </div>
+  </div>
+  <div class="footer-col-two">
+    <h4>Product</h4>
+    <div class="footer-navigations">
+        <p>Home</p>
+        <p>Pricing</p>
+        <p>Testimony</p>
+        <p>Careers </p>
+    </div>
+  </div>
 </footer>
 
 <style>
+    footer{
+        display:grid;
+        grid-template-columns: repeat(3, 1fr);
+        padding: 1.2rem;
+        gap: 2rem;
+    }
+    .footer-col-two, .footer-navigations{
+        display: flex;
+        flex-direction: column;
+        gap: .8rem;
+    }
+    .footer-navigations p{
+        font-size: 1.18rem;
+        font-weight: 700;
+    }
+    .wrapper{
+        display: flex;
+        gap: .5rem;
+        align-items: center;
+    }
+    span{
+        background-color: black;
+        color: white;
+        font-size: .84rem;
+        padding: 3px;
+        border-radius: 30%;
+    }
 </style>
